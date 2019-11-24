617ce86754ab474d73a68c60983bd8c727fc0d81
Nick Kunz <nick.kunz@columbia.edu>
2019-11-23 20:10:56 -0800

    Import revisions, version 0.0.4

---
+++ smogn/__init__.py
@@ -0,0 +1,15 @@
+"""
+Synthetic Minority Over-Sampling Technique for Regression with Gaussian Noise
+
+https://github.com/nickkunz/smogn
+"""
+
+__all__ = [
+
+    "box_plot_stats", 
+    "dist_metrics",
+    "over_sampling",
+    "phi_ctrl_pts",
+    "phi",
+    "smogn"
+]

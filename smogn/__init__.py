cd375ed118822820d0ede5c9da98374342ef6922
Nick Kunz <nick.kunz@columbia.edu>
2019-11-26 22:30:00 -0600

    Fix import path 0.0.5

--- smogn/__init__.py
+++ smogn/__init__.py
@@ -5,18 +5,18 @@ https://github.com/nickkunz/smogn
 """
 
 from smogn.box_plot_stats import box_plot_stats
-from dist_metrics import euclidean_dist
-from dist_metrics import heom_dist
-from dist_metrics import overlap_dist
-from over_sampling import over_sampling
-from phi_ctrl_pts import phi_ctrl_pts
-from phi_ctrl_pts import phi_extremes
-from phi_ctrl_pts import phi_range
-from phi import phi
-from phi import phi_init
-from phi import pchip_slope_mono_fc
-from phi import pchip_val
-from smoter import smoter
+from smogn.dist_metrics import euclidean_dist
+from smogn.dist_metrics import heom_dist
+from smogn.dist_metrics import overlap_dist
+from smogn.over_sampling import over_sampling
+from smogn.phi_ctrl_pts import phi_ctrl_pts
+from smogn.phi_ctrl_pts import phi_extremes
+from smogn.phi_ctrl_pts import phi_range
+from smogn.phi import phi
+from smogn.phi import phi_init
+from smogn.phi import pchip_slope_mono_fc
+from smogn.phi import pchip_val
+from smogn.smoter import smoter
 
 
 __all__ = [

7534a49d09737c7d2affd6042c689be9499c9ec2
Nick Kunz <nick.kunz@columbia.edu>
2019-12-06 17:49:16 -0600

    Correct indentation: phi_range()

--- smogn/phi_ctrl_pts.py
+++ smogn/phi_ctrl_pts.py
@@ -148,7 +148,7 @@ def phi_range(y, xtrm_type, coef, ctrl_pts):
     
     ## quality control checks for user specified phi relevance values
     if np.isnan(ctrl_pts).any() or np.size(ctrl_pts, axis = 1) > 3 or np.size(
-      ctrl_pts, axis = 1) < 2 or not isinstance(ctrl_pts, (np.ndarray)):
+    ctrl_pts, axis = 1) < 2 or not isinstance(ctrl_pts, np.ndarray):
         print("ctrl_pts must be given as a matrix in the form: [x, y, m]" 
               "or [x, y]")
     

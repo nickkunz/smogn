443a22df83e66ada5c752694392b90046ef40e61
Nick Kunz <nick.kunz@columbia.edu>
2019-12-18 14:05:14 -0600

    Remove import unused functions

--- smogn/__init__.py
+++ smogn/__init__.py
@@ -4,13 +4,6 @@ Synthetic Minority Over-Sampling Technique for Regression with Gaussian Noise
 https://github.com/nickkunz/smogn
 """
 
-from smogn.box_plot_stats import box_plot_stats
-from smogn.dist_metrics import euclidean_dist, heom_dist, overlap_dist
-from smogn.over_sampling import over_sampling
-from smogn.phi_ctrl_pts import phi_ctrl_pts, phi_extremes, phi_range
-from smogn.phi import phi, phi_init, pchip_slope_mono_fc, pchip_val
-from smogn.smoter import smoter
-
 __all__ = [
 
     "box_plot_stats",
